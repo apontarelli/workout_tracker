7089946146f491b1c1122c08dc8b448b39185679
Antonio Pontarelli <apontarelli@wrapbook.com>
2024-05-22 22:15:09 -0700

    Create exercises helper spec

---
+++ spec/helpers/exercises_helper_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ExercisesHelper, type: :helper do
+  let(:user) { User.create!(name: 'John Doe', email: 'test@example.com', password: 'password', password_confirmation: 'password') }
+
+  before do
+    Exercise.delete_all
+    UserExercise.delete_all
+
+    @global_exercise = Exercise.create!(name: 'Push Up', instructions: 'A basic push-up exercise', primary_muscle_group: 'Chest', secondary_muscle_groups: ['Triceps'], equipment: 'None', exercise_group: 'Strength')
+    @user_exercise = UserExercise.create!(name: 'User Exercise', primary_muscle_group: 'Legs', secondary_muscle_groups: ['Glutes'], equipment: 'Dumbbells', exercise_group: 'Strength', user: user)
+  end
+
+  describe "#combined_exercises" do
+    it "returns a combined list of global and user exercises" do
+      result = helper.combined_exercises(user)
+      expected_result = [
+        ["Push Up", "Exercise-#{@global_exercise.id}"],
+        ["User Exercise", "UserExercise-#{@user_exercise.id}"]
+      ]
+
+      expect(result).to match_array(expected_result)
+    end
+  end
+end
